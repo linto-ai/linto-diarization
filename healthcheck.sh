557e68606f9b603b8be78b4cc3739e85304c5085
damienlaine <damien.laine@gmail.com>
2024-06-17 11:12:40 +0200

    Add health check script for Celery worker with GPU utilization check. Shall fix unregistering long running tasks as well

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,9 +6,26 @@ if [ "$SERVICE_MODE" = "http" ]
 then
     curl --fail http://localhost:80/healthcheck || exit 1
 else
-    # Update last alive
-    python -c "from celery_app.register import register; register()"
-    
-    # Ping worker
-    celery --app=celery_app.celeryapp inspect ping -d diarization_worker@$HOSTNAME || exit 1
+    # Update last alive, using Heartbeat(register(True))
+    python -c "from celery_app.register import register; register(True)"
+
+    # Check if Celery worker process is running
+    # warning : We might rework this when switching to non-root user. Which is required for security reasons.
+    # Our docker images are currently running as root and this is bad :)
+    if ! pgrep -f "celery worker"; then
+        echo "HealtchCheck FAIL : Celery worker process not running"
+        exit 1
+    fi
+
+    # Check GPU utilization
+    if nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | grep -v '^0$'; then
+        #"GPU is being utilized, assuming healthy"
+        exit 0
+    else
+        # If GPU is not being utilized, attempt a ping as a secondary check
+        if ! celery --app=celery_app.celeryapp inspect ping -d ${SERVICE_NAME}_worker@$HOSTNAME --timeout=20; then
+            echo "HealtchCheck FAIL : Celery worker not responding in time and GPU is not being utilized"
+            exit 1
+        fi
+    fi
 fi
