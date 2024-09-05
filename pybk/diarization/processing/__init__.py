869f61e3541607d8d0fa79bfc3a2b7540b0f13ea
Jeronymous <jeromelouradour@gmail.com>
2024-09-05 08:30:25 +0200

    Let PyBK be without identification (legacy)

--- pybk/diarization/processing/__init__.py
+++ pybk/diarization/processing/__init__.py
@@ -1,12 +1,4 @@
-import os
-
-import torch
-
-device = os.environ.get("DEVICE")
-if device is None:
-    USE_GPU = torch.cuda.is_available()
-else:
-    USE_GPU = device != "cpu"
+USE_GPU = False
 
 from .speakerdiarization import SpeakerDiarization
 
