d52ff74b17e27fcf46d3e4c633ca8f7332055abe
damienlaine <damien.laine@gmail.com>
2024-10-29 01:02:55 +0100

    Simplest healthcheck without heavy rework

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 
 set -eax
+# Put last healthcheck logs in an file (You might persist this for healthcheck failures analysis as the container is ephemeral)
+# Otherwise healthcheck logs are found in docker inspect <container_id>
+
+exec > /tmp/healthcheck.log 2>&1
 
 if [ "$SERVICE_MODE" = "http" ]
 then
+    # HTTP mode healthcheck
     curl --fail http://localhost:80/healthcheck || exit 1
 else
     # Update last alive
     python -c "from celery_app.register import register; register(False)"
 
     # Check if Celery worker process is running
-    # warning : We might rework this when switching to non-root user. Which is required for security reasons.
-    # Our docker images are currently running as root and this is bad :)
     PID=`pgrep -f "celeryapp worker"`
     if [ -z "$PID" ]; then
-        echo "HealtchCheck FAIL : Celery worker process not running"
+        echo "HealthCheck FAIL: Celery worker process not running"
         exit 1
     fi
 
-    # Check if GPU is in used
-    has_gpu=$(nvidia-smi --query-compute-apps pid --format=csv,noheader | wc -l)
-    # Note: we should add a check on the PID ("| grep $PID")... BUT the PID can be different in the container than on the host
+    # Check if GPU is in use
+    has_gpu=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | grep -v '^0$')
+    if [ "$has_gpu" -gt 0 ]; then
+        echo "HealthCheck PASS: GPU is being utilized, marking service as healthy."
+        exit 0
+    fi
 
-    # Attempt a ping
+    # Attempt to ping Celery worker
     if ! celery --app=celery_app.celeryapp inspect ping -d ${SERVICE_NAME}_worker@$HOSTNAME --timeout=20; then
-        # Check GPU utilization
-        if [ $has_gpu -gt 0 ]; then
-            # GPU is being utilized, assuming healthy
-            echo "Celery worker not responding in time but GPU is being utilized (trying to ping again)"
-            exit 0
-        fi
-        echo "HealtchCheck FAIL : Celery worker not responding in time and GPU is not being utilized"
+        echo "HealthCheck FAIL: Celery worker not responding in time and GPU is not being utilized"
         exit 1
     fi
+
+    echo "HealthCheck PASS: Celery worker is responsive but idle, marking service as healthy."
+    exit 0
 fi
